88df1ac6d98b458ef72c356d024b82690746e910
VarleyS <varleysr@gmail.com>
2026-01-29 23:42:53 -0300

    Trabalho4 matriz de identidade.

---
+++ Trabalho4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("\t\t Varley Ribeiro\n\t Conquistando os desafios em C#");
+
+int tamanho;
+
+Console.WriteLine("Informe o tamanho da Matrix Identidade: ");
+tamanho = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("\n\n Matrix Indetidade\n\n");
+
+for (int i = 0; i < tamanho; i++)
+{
+    for (int j = 0; j < tamanho; j++)
+    {
+        if (i == j)
+        {
+            Console.Write(1);
+        }
+        else
+        {
+            Console.Write(0);
+        }
+    }
+    Console.WriteLine("\n");
+}
+Console.ReadKey();
