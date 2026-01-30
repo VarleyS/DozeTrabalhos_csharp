85d5c9ac245b0ed693a8b3b6efdb0d2e8426776e
VarleyS <varleysr@gmail.com>
2026-01-30 15:57:05 -0300

    Trabalho7 concluido

---
+++ Trabalho7/Program.cs
@@ -0,0 +1,24 @@
+﻿//Escreva um programa que receba n valores do usuário e em seguida ordene os valores em ordem ascendente.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Quantos valores você deseja inserir? ");
+        int n = int.Parse(Console.ReadLine());
+        int[] valores = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Digite o valor {i + 1}: ");
+            valores[i] = int.Parse(Console.ReadLine());
+        }
+        Array.Sort(valores);
+        Console.WriteLine("Valores em ordem ascendente:");
+        foreach (int valor in valores)
+        {
+            Console.WriteLine(valor);
+        }
+    }
+}
